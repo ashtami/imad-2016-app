4c495fdc2c5460d68401007804da54d67f23bc59
ashtami <ashtamim@gmail.com>
2016-10-21 14:22:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
 app.get('/taxcalc.html', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'taxcalc.html'));
 });
